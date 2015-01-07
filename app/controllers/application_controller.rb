a4059baa58fd8bf60261ec2129f1c2816f536d4b
elovell <ed.lovell@its.ny.gov>
2015-01-07 15:23:19 -0500

    First Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # Test comment
   protect_from_forgery with: :exception
   include Pundit
 end
